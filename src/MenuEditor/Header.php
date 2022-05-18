4eb0f623977d8d4e7aa1915625ad130467087f72
d.savuljesku <d.savuljesku@gmail.com>
2022-05-18 16:23:19 +0200

    Integrate into MenuEditor
    [4.2]
    
    ERM 27548
    
    Change-Id: I936d85630ac58b98673e692c850a87a87ce81486
    (cherry picked from commit 0266105477764276997c661af8b24c03eb55a215)
    (cherry picked from commit 5b64b672a4975dbae9cc80c4804cfcf59248b2bd)

---
+++ src/MenuEditor/Header.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BlueSpice\CustomMenu\MenuEditor;
+
+use MediaWiki\Extension\MenuEditor\Menu\MediawikiSidebar;
+use Title;
+
+class Header extends MediawikiSidebar {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function appliesToTitle( Title $title ): bool {
+		return $title->getNamespace() === NS_MEDIAWIKI &&
+			$title->getDBkey() === 'CustomMenu/Header';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModule(): string {
+		return 'ext.bluespice.custom-menu.menueditor';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getJSClassname(): string {
+		return 'ext.custommenu.menueditor.HeaderTree';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getKey(): string {
+		return 'custommenu';
+	}
+}
